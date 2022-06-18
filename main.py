4372e6eaf0ffdd209615a589dd090daa15019594
ROHIT SAROJ <70078572+Rohit7594@users.noreply.github.com>
2022-06-18 09:14:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+from flask import Flask, render_template,request
+import pickle
+
+app=Flask(__name__)
+data=pd.read_csv("Data/Clean_data.csv")
+pipe=pickle.load(open("Ridge_Model.pkl","rb"))
+
+@app.route('/')
+def index():
+
+    locations=sorted(data["location"].unique())
+    return render_template('index.html', locations=locations)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    locations=request.form.get('location')
+    bhk = float(request.form.get('Bhk'))
+    bath = float(request.form.get('bath'))
+    sqft = float(request.form.get('total_sqft'))
+    print(locations,bhk,bath,sqft)
+
+    input=pd.DataFrame([[locations,sqft,bath,bhk]],columns=["location","total_sqft","bath","Bhk"])
+    prediction=pipe.predict(input)[0]*100000
+    return str(np.round(prediction,2))
+
+if __name__=="__main__":
+    app.run(debug=True,port=5000)
